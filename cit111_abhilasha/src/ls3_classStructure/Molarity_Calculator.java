61b5a70fff96c2d6219fbcc58c0508f4961f5ae7
Raghuwanshi <abhilasha.raghuwansh@ccac.org>
2019-11-06 13:18:07 -0500

    Molarity Calculator

--- cit111_abhilasha/src/ls3_classStructure/Molarity_Calculator.java
+++ cit111_abhilasha/src/ls3_classStructure/Molarity_Calculator.java
@@ -5,18 +5,33 @@
  */
 package ls3_classStructure;
 
+import java.util.Scanner;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
  
 /**
  * The calculator computes the molarity of a given solution
  * @author abhilasha.raghuwansh
  */
 public class Molarity_Calculator {
-    public static void main(String[] args){
+    
+
         
+    public static void main(String[] args){
+        takeUserInput();
     }//close main
     
-    public static void eleMolarMass(){
+    public static void takeUserInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter formula of the Compound");
+        String formula = input.next();
+        parseFormula(formula);
+    }//close method takeUserInput
+    
+    
+    public static void parseFormula(String formula){
+        
         HashMap<String , Double> eleMap = new HashMap<>();
         eleMap.put("H", 1.00797);
         eleMap.put("He", 4.00260);
@@ -33,6 +48,135 @@ public class Molarity_Calculator {
         eleMap.put("Al", 26.98154);
         eleMap.put("Si", 28.0855);
         eleMap.put("P", 30.97376);
+        eleMap.put("S", 32.06);
+        eleMap.put("Cl", 35.453);
+        eleMap.put("K", 39.0983);
+        eleMap.put("Ar", 39.948);
+        eleMap.put("Ca", 40.08);
+        eleMap.put("Sc", 44.9559);
+        eleMap.put("Ti", 47.90);
+        eleMap.put("V", 50.9380);
+        eleMap.put("Cr", 51.996);
+        eleMap.put("Mn", 54.9380);
+        eleMap.put("Fe", 55.847);
+        eleMap.put("Ni", 58.70);
+        eleMap.put("Co", 58.9332);
+        eleMap.put("Zn", 65.38);
+        eleMap.put("Ga", 69.72);
+        eleMap.put("Ge", 72.59);
+        eleMap.put("Ar", 74.9216);
+        eleMap.put("Se", 78.96);
+        eleMap.put("Br", 79.904);
+        eleMap.put("Kr", 83.80);
+        eleMap.put("Rb", 85.4678);
+        eleMap.put("Sr", 87.62);
+        eleMap.put("Y", 88.9059);
+        eleMap.put("Zr", 91.22);
+        eleMap.put("Nb", 92.9064);
+        eleMap.put("Mo", 95.94);
+        eleMap.put("Tc", 98.00);
+        eleMap.put("Ru", 101.07);
+        eleMap.put("Rh", 102.9055);
+        eleMap.put("Pd", 106.4);
+        eleMap.put("Ag", 107.868);
+        eleMap.put("Cd", 112.41);
+        eleMap.put("In", 114.82);
+        eleMap.put("Sn", 118.69);
+        eleMap.put("Sb", 121.75);
+        eleMap.put("I", 126.9045);
+        eleMap.put("Te", 127.60);
+        eleMap.put("Xe", 131.30);
+        eleMap.put("Cs", 132.9054);
+        eleMap.put("Ba", 137.33);
+        eleMap.put("La", 138.9055);
+        eleMap.put("Ce", 140.12);
+        eleMap.put("Pr", 140.9077);
+        eleMap.put("Nd",144.24);
+        eleMap.put("Pm", 145.00);
+        eleMap.put("Sm", 150.4);
+        eleMap.put("Eu", 151.96);
+        eleMap.put("Gd", 157.25);
+        eleMap.put("Tb", 158.9254);
+        eleMap.put("Dy", 162.50);
+        eleMap.put("Ho", 164.9304);
+        eleMap.put("Er", 167.26);
+        eleMap.put("Tm", 168.9342);
+        eleMap.put("Yb", 173.04);
+        eleMap.put("Lu", 174.967);
+        eleMap.put("Hf", 178.49);
+        eleMap.put("Ta", 180.9479);
+        eleMap.put("W", 183.85);
+        eleMap.put("Re",186.207);
+        eleMap.put("Os", 190.2);
+        eleMap.put("Ir", 192.22);
+        eleMap.put("Pt", 195.09);
+        eleMap.put("Au", 196.9665);
+        eleMap.put("Hg", 200.59);
+        eleMap.put("Tl", 204.37);
+        eleMap.put("Pb", 207.2);
+        eleMap.put("Bi", 208.9804);
+        eleMap.put("Po", 209.00);
+        eleMap.put("At", 210.00);
+        eleMap.put("Rn", 222.00);
+        eleMap.put("Fr", 223.00);
+        eleMap.put("Ra", 226.0254);
+        eleMap.put("Ac", 227.0278);
+        eleMap.put("Pa", 231.0359);
+        eleMap.put("Th", 232.0381);
+        eleMap.put("Np", 237.0482);
+        eleMap.put("U", 238.029);
+        eleMap.put("Pu", 242.00);
+        eleMap.put("Am", 243.00);
+        eleMap.put("Bk", 247.00);
+        eleMap.put("Cm", 247.00);
+        eleMap.put("No", 250.00);
+        eleMap.put("Cf", 251.00);
+        eleMap.put("Es", 252.00);
+        eleMap.put("Hs", 255.00);
+        eleMap.put("Mt", 256.00);
+        eleMap.put("Fm", 257.00);
+        eleMap.put("Md", 258.00);
+        eleMap.put("Lr", 260.00);
+        eleMap.put("Rf", 261.00);
+        eleMap.put("Bh", 262.00);
+        eleMap.put("Db", 262.00);
+        eleMap.put("Sg", 263.00);
+        eleMap.put("Uun", 269.00);
+        eleMap.put("Uuu", 272.00);
+        eleMap.put("Uub", 277.00);
+
+        HashMap<String , Double> eleSubMap = new HashMap<>();
         
-    }//close eleMolarMass
+        
+        
+        
+        
+        Pattern a = Pattern.compile("[A-Z][a-z]?[a-z]?\\d?");
+        //find the match(element)
+        Matcher m = a.matcher(formula);
+        double eleAtomicMass = 0;
+            
+        while(m.find() == true){
+            //obtain element from the formula using substring
+            String element = formula.substring(m.start(), m.end());
+            System.out.println(element);
+            
+            //check to see if the element has a subscript
+            Pattern b = Pattern.compile("\\d");
+            Matcher n = b.matcher(element);
+            if(n.find() == true){
+                
+            }
+            //find the atomic mass of the element
+            eleAtomicMass = eleMap.get(element);
+            System.out.println(eleAtomicMass);
+            
+            //chop off the derived element from the formula
+            formula = formula.substring(m.end(), formula.length());
+            System.out.println(formula);
+            
+            m = a.matcher(formula);
+        }    
+    }// close method parseFormula
+    
 }//close class
